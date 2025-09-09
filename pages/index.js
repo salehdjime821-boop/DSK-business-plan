80199d2bd04c3d6eb1453c220ba188c6fc7fcfa0
salehdjime821-boop <salehdjime821@gmail.com>
2025-09-09 14:31:39 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🚀 Welcome to My Next.js App</h1>
+
       <p>This is the homepage deployed on Vercel.</p>
+
       <p>🌍 Bonjour! Ceci est la page d'accueil.</p>
     </div>
   );
-}
+} 
